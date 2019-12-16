64665dbb825822a4ed652b64d497d7c4f25e2954
achen228 <achen228@163.com>
2019-12-16 20:26:19 +0800

    Update Test.cpp

--- 小游戏/简单的三子棋游戏/Test.cpp
+++ 小游戏/简单的三子棋游戏/Test.cpp
@@ -239,3 +239,18 @@ void Game()
 	}
 }
 
+
+int Menu()
+{
+	printf("===============\n");
+	printf("  三子棋游戏\n");
+	printf("  1.开始游戏\n");
+	printf("  0.结束游戏\n");
+	printf("===============\n");
+	int choice;
+	printf("请输入您的选项：");
+	scanf("%d", &choice);
+	return choice;
+}
+
+
